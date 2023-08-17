34d9223f2763f24fa7a07ee27df3c8e347257370
rajyalakshmi <rajianna239@gmail.com>
2023-08-17 21:50:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from typing import Any, Dict
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+class cbv_context(TemplateView):
+    template_name='cbv_context.html'
+
+    def get_context_data(self, **kwargs) :
+        EDCO=super().get_context_data(**kwargs)
+        
+        EDCO['name']="kalyani"
+        EDCO['age']=21
+        return EDCO
